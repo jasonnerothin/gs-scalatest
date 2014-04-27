d45dde96e303112331cac6e698e38f38eb9affb3
Jason Nerothin <jason.nerothin@kohls.com>
2014-04-27 03:51:25 -0500

    something ain't right here

--- src/test/scala/com/gigaspaces/toymodel/DomainModelReadWriteSuite.scala
+++ src/test/scala/com/gigaspaces/toymodel/DomainModelReadWriteSuite.scala
@@ -17,12 +17,14 @@
 package com.gigaspaces.toymodel
 
 import com.gigaspaces.GsI10nSuite
-import com.gigaspaces.query.IdQuery
+import com.gigaspaces.query.{ISpaceQuery, IdQuery}
 import com.gigaspaces.toymodel.Salary
 import org.scalatest.ConfigMap
-import com.sun.servicetag.SystemEnvironment
 import scala.sys.SystemProperties
 import java.util.NoSuchElementException
+import com.gigaspaces.client.{ChangeModifiers, ChangeResult, ChangeSet}
+import java.util.concurrent.Future
+import com.gigaspaces.async.AsyncFutureListener
 
 /** Created by IntelliJ IDEA.
   * User: jason
@@ -56,6 +58,294 @@ class DomainModelReadWriteSuite extends GsI10nSuite {
     val readBack = gigaSpace.readById(new IdQuery(classOf[Salary], id))
 
     assert(readBack === salary)
+
+  }
+
+  ignore("salary asyncChange(ISpaceQuery, ChangeSet)") {
+    val spaceQuery: ISpaceQuery[Salary] = makeSpaceQuery()
+    val changeSet: ChangeSet = makeChangeSet()
+    val result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(spaceQuery, changeSet)
+  }
+
+  def makeSpaceQuery(): ISpaceQuery[Salary] = {
+    null
+  }
+
+  def makeChangeSet(): ChangeSet[Salary] = {
+    null
+  }
+
+  ignore("salary asyncChange(Salary, ChangeSet)") {
+    val salary: Salary = makeSalary()
+    val changeSet: ChangeSet = makeChangeSet()
+    val result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(salary, changeSet)
+  }
+
+  def makeSalary(): Salary = {
+    null
+  }
+
+  ignore("salary asyncChange(ISpaceQuery, ChangeModifiers, AsyncFutureListener[ChangeResult[Salary]])") {
+
+    val spaceQuery = makeSpaceQuery()
+    val changeSet = makeChangeSet()
+    val listener = makeAsyncFutureListener()
+
+    val changeModifiers0 = ChangeModifiers.MEMORY_ONLY_SEARCH
+    val changeModifiers1 = ChangeModifiers.NONE
+    val changeModifiers2 = ChangeModifiers.ONE_WAY
+    val changeModifiers3 = ChangeModifiers.RETURN_DETAILED_RESULTS
+
+
+    var result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers0, listener)
+    // verify(listener.gotPinged)
+
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers1, listener)
+    // verify(listener.gotPinged)
+
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers2, listener)
+    // verify(listener.gotPinged)
+
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers3, listener)
+    // verify(listener.gotPinged)
+
+  }
+
+  def makeAsyncFutureListener(): AsyncFutureListener[ChangeResult[Salary]] = {
+    null
+  }
+
+  ignore("salary asyncChange(ISpaceQuery, ChangeModifiers, Long, AsyncFutureListener[ChangeResult[Salary]])") {
+
+    val spaceQuery = makeSpaceQuery()
+    val changeSet = makeChangeSet()
+    val listener = makeAsyncFutureListener()
+    val timeout: Long = 39
+    val changeModifiers0 = ChangeModifiers.MEMORY_ONLY_SEARCH
+    val changeModifiers1 = ChangeModifiers.NONE
+    val changeModifiers2 = ChangeModifiers.ONE_WAY
+    val changeModifiers3 = ChangeModifiers.RETURN_DETAILED_RESULTS
+
+    var result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers0, timeout, listener)
+    // verify(listener.gotPinged)
+
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers1, timeout, listener)
+    // verify(listener.gotPinged)
+
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers2, timeout, listener)
+    // verify(listener.gotPinged)
+
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers3, timeout, listener)
+    // verify(listener.gotPinged)
+
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers4, timeout, listener)
+    // verify(listener.gotPinged)
+
+  }
+
+  ignore("salary asyncChange(Salary, ChangeSet[Salary], ChangeModifiers)") {
+
+    val salary: Salary = makeSalary()
+    val changeSet = makeChangeSet()
+    val listener = makeAsyncFutureListener()
+
+    val result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(salary, changeSet, listener)
+
+  }
+
+  ignore("salary asyncChange( ChangeSet[Salary], AsyncFutureListener[ChangeResult[Salary]])") {
+
+    val changeSet = makeChangeSet()
+    val listener = makeAsyncFutureListener()
+
+    val result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(changeSet, listener)
+
+  }
+
+  ignore("salary asyncChange( ChangeSet[Salary], ChangeModifiers, Long, AsyncFutureListener[ChangeResult[Salary]])") {
+
+    val changeSet = makeChangeSet()
+    val listener = makeAsyncFutureListener()
+    val timeout: Long = 39
+
+    val changeModifiers0 = ChangeModifiers.MEMORY_ONLY_SEARCH
+    val changeModifiers1 = ChangeModifiers.NONE
+    val changeModifiers2 = ChangeModifiers.ONE_WAY
+    val changeModifiers3 = ChangeModifiers.RETURN_DETAILED_RESULTS
+
+    var result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(changeSet, changeModifiers0, timeout, listener)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers1, timeout, listener)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers2, timeout, listener)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers3, timeout, listener)
+    // verify(listener.gotPinged)
+
+  }
+
+  ignore("salary asyncChange(ChangeSet[Salary], Long)") {
+
+    val changeSet = makeChangeSet()
+    val timeout: Long = 39
+
+    val result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(changeSet, timeout)
+
+  }
+
+  ignore("salary asyncChange(ChangeSet[Salary], Long, AsyncFutureListener[ChangeResult[Salary]])") {
+
+    val changeSet = makeChangeSet()
+    val timeout: Long = 39
+    val listener = makeAsyncFutureListener()
+
+    val result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(changeSet, timeout, listener)
+
+  }
+
+  ignore("salary asyncChange(ChangeSet[Salary], Long, AsyncFutureListener[ChangeResult[Salary]])") {
+
+    val changeSet = makeChangeSet()
+    val listener = makeAsyncFutureListener()
+    val timeout: Long = 39
+
+    val result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(changeSet, timeout, listener)
+
+  }
+
+  ignore("salary asyncChange(ChangeSet[Salary], ChangeModifiers, AsyncFutureListener[ChangeResult[Salary]])") {
+
+    val changeSet = makeChangeSet()
+    val listener = makeAsyncFutureListener()
+
+    val changeModifiers0 = ChangeModifiers.MEMORY_ONLY_SEARCH
+    val changeModifiers1 = ChangeModifiers.NONE
+    val changeModifiers2 = ChangeModifiers.ONE_WAY
+    val changeModifiers3 = ChangeModifiers.RETURN_DETAILED_RESULTS
+
+    var result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(changeSet, changeModifiers0, listener)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers0, listener)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers1, listener)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers2, listener)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers3, listener)
+    // verify(listener.gotPinged)
+
+  }
+
+  ignore("salary asyncChange(ChangeSet[Salary], ChangeModifiers)") {
+
+    val changeSet = makeChangeSet()
+    val changeModifiers0 = ChangeModifiers.MEMORY_ONLY_SEARCH
+    val changeModifiers1 = ChangeModifiers.NONE
+    val changeModifiers2 = ChangeModifiers.ONE_WAY
+    val changeModifiers3 = ChangeModifiers.RETURN_DETAILED_RESULTS
+
+    var result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(changeSet, changeModifiers0)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers0)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers1)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers2)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(changeSet, changeModifiers3)
+    // verify(listener.gotPinged)
+
+  }
+
+  ignore("salary asyncChange(ISpaceQuery[Salary], ChangeSet[Salary], ChangeModifiers)") {
+
+    val spaceQuery: ISpaceQuery[Salary] = makeSpaceQuery()
+    val changeSet = makeChangeSet()
+    val changeModifiers0 = ChangeModifiers.MEMORY_ONLY_SEARCH
+    val changeModifiers1 = ChangeModifiers.NONE
+    val changeModifiers2 = ChangeModifiers.ONE_WAY
+    val changeModifiers3 = ChangeModifiers.RETURN_DETAILED_RESULTS
+
+    var result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers0)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers1)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers2)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers3)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers4)
+    // verify(listener.gotPinged)
+
+  }
+
+  ignore("salary asyncChange(ISpaceQuery[Salary], ChangeSet[Salary], Long, ChangeModifiers)") {
+
+    val spaceQuery = makeSpaceQuery()
+    val changeSet = makeChangeSet()
+    val timeout: Long = 23
+    val changeModifiers0 = ChangeModifiers.MEMORY_ONLY_SEARCH
+    val changeModifiers1 = ChangeModifiers.NONE
+    val changeModifiers2 = ChangeModifiers.ONE_WAY
+    val changeModifiers3 = ChangeModifiers.RETURN_DETAILED_RESULTS
+
+    var result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(spaceQuery, changeSet, timeout, changeModifiers0)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, timeout, changeModifiers1)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, timeout, changeModifiers2)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, timeout, changeModifiers3)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, timeout, changeModifiers4)
+
+  }
+
+  ignore("salary asyncChange(Salary, ChangeSet[Salary], ChangeModifiers, Long)") {
+
+    val salary: Salary = makeSalary()
+    val changeSet = makeChangeSet()
+    val timeout: Long = 23
+    val changeModifiers0 = ChangeModifiers.MEMORY_ONLY_SEARCH
+    val changeModifiers1 = ChangeModifiers.NONE
+    val changeModifiers2 = ChangeModifiers.ONE_WAY
+    val changeModifiers3 = ChangeModifiers.RETURN_DETAILED_RESULTS
+
+    var result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(salary, changeSet, changeModifiers0, timeout)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(salary, changeSet, changeModifiers0, timeout)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(salary, changeSet, changeModifiers1, timeout)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(salary, changeSet, changeModifiers2, timeout)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(salary, changeSet, changeModifiers3, timeout)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(salary, changeSet, changeModifiers4, timeout)
+    // verify(listener.gotPinged)
+
+  }
+
+  ignore("salary asyncChange(ISpaceQuery[Salary], ChangeSet[Salary], ChangeModifiers, Long"){
+
+    val spaceQuery = makeSpaceQuery()
+    val changeSet = makeChangeSet()
+    val timeout: Long = 23
+    val changeModifiers0 = ChangeModifiers.MEMORY_ONLY_SEARCH
+    val changeModifiers1 = ChangeModifiers.NONE
+    val changeModifiers2 = ChangeModifiers.ONE_WAY
+    val changeModifiers3 = ChangeModifiers.RETURN_DETAILED_RESULTS
+
+    var result: Future[ChangeResult[Salary]] = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers0, timeout)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers1, timeout)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers2, timeout)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers3, timeout)
+    // verify(listener.gotPinged)
+    result = gigaSpace.asyncChange(spaceQuery, changeSet, changeModifiers4, timeout)
+    // verify(listener.gotPinged)
 
   }
 
